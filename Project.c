1e56a7319456f4b3709d41b414c3f7f2ca80ca57
deeztrzl <148042421+deeztrzl@users.noreply.github.com>
2024-01-03 19:30:32 +0700

    Update Project.c
    menambahkan function pemasukan

--- Project.c
+++ Project.c
@@ -19,3 +19,21 @@ struct AkumulasiKategori {
     char nama[50];
     double totalJumlah;
 };
+
+void inputPemasukan(double *totalPemasukan, struct AkumulasiKategori kategori[], int *jumlahKategori) {
+    double pemasukan;
+    int test;
+
+    do {
+        printf("Masukkan pemasukan (masukkan 0 untuk kembali ke menu utama): ");
+        test = scanf("%lf", &pemasukan);
+
+        if (test == 0) {
+            printf("Mohon masukkan angka untuk input.\n");
+            fflush(stdin);
+
+        } else if (pemasukan != 0) {
+            *totalPemasukan += pemasukan;
+        }
+    } while (pemasukan != 0);
+}
