015c8754e10a520acbb25a0029c96dda3bd823f6
Excel <148018349+valerianharyanto@users.noreply.github.com>
2024-01-03 18:17:49 +0700

    Update Excel - Project.c

--- Project.c
+++ Project.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define CATEGORIES_MAX 99
+#define CATEGORIES_NAMA 99
 
+struct Transaksi {
+    char kategori[50];
+    double jumlah;
+};
+
+struct PersentaseKategori {
+    char nama[50];
+    float persentase;
+};
+
+struct AkumulasiKategori {
+    char nama[50];
+    double totalJumlah;
+};
